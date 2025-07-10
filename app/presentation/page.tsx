b0f59d8223225576b7b17213b570c8c28e62913e
devOluwaDamilare <bolarinwaoluwadamilare91@gmail.com>
2025-07-09 21:42:09 -0700

    Change height

--- app/presentation/page.tsx
+++ app/presentation/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
         <iframe
           src="https://docs.google.com/presentation/d/1Ysuy8IcR88FiufY3UUXpNhcg9PXj8Au-/edit?usp=drivesdk&ouid=102493840241668193229&rtpof=true&sd=true"
           width="100%"
-          height="100%"
+          height="800px"
         />
       </div>
     </main>
